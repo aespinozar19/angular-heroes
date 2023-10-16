c655fbc18c8409d40b3a5a8ac5ad6e00f5d40b15
Anthony Espinoza <aespinozar19@gmail.com>
2023-10-15 22:58:11 -0500

    material and flex style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
     SharedModule,
   ],
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+
+
+export const environments = {
+  baseUrl: 'http://hosting.com:3000/api'
+}
